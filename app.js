f1d2b2c07472d2c58c5d22948902ce81c2119f35
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-06-15 12:16:23 +0500

    chore(app): initialize router and view engine
    - Load environment variables securely
    - Configure EJS as the default view engine
    - Mount the /company route to the application
    - Add global error handling middleware

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const { loadEnvFile } = require("node:process");
+const path = require("node:path");
+loadEnvFile();
 app = express();
+const companyRouter = require("./routes/companyRoute");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/company", companyRouter);
+app.use((error, req, res, next) => {
+  console.log(error);
+});
 
 app.listen(3000, () => {
   console.log(`Server listening on http://localhost:3000`);
