07a2dcb53e4b3ba550c851bbd280f84baf9d46b7
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-06-15 13:53:03 +0500

    chore(app): initialize router for parts

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ app.set("view engine", "ejs");
 
 const companyRouter = require("./routes/companyRoute");
 const carsRouter = require("./routes/carsRoute");
+const partsRouter = require("./routes/partsRoute");
 
 app.use("/company", companyRouter);
 
 app.use("/cars", carsRouter);
+
+app.use("/parts", partsRouter);
 
 app.use((error, req, res, next) => {
   console.log(error);
